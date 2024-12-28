55e2c2c2dd6d66d236bf2a1d06a23097eb26a9c2
ray <raydatray@gmail.com>
2024-12-27 23:12:21 -0500

    registration response extended, new endpoint

--- ecse-321-demo-backend/src/main/java/com/example/ecse_321_demo_backend/responses/RegistrationResponse.java
+++ ecse-321-demo-backend/src/main/java/com/example/ecse_321_demo_backend/responses/RegistrationResponse.java
@@ -1,6 +1,7 @@
 package com.example.ecse_321_demo_backend.responses;
 
 import com.example.ecse_321_demo_backend.models.Registration;
+import java.sql.Timestamp;
 import java.util.UUID;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ public class RegistrationResponse {
     private String username;
     private UUID eventId;
     private String eventDescription;
+    private Timestamp registeredAt;
 
     public static RegistrationResponse fromRegistration(
         Registration registration
@@ -20,6 +22,7 @@ public class RegistrationResponse {
         response.setUsername(registration.getUser().getUsername());
         response.setEventId(registration.getEvent().getId());
         response.setEventDescription(registration.getEvent().getDescription());
+        response.setRegisteredAt(registration.getRegistrationAt());
         return response;
     }
 }
